b604b1afe0cf29912c5818490ef7874881b059b0
jayesh9508 <jayesh9508@gmail.com>
2017-12-29 17:15:17 +0530

    name change

--- Day3/ViewModels/EmpViewModel.cs
+++ Day3/ViewModels/EmpViewModel.cs
@@ -18,7 +18,7 @@ namespace Day3.ViewModels
             get { return Emp.FirstName; }
             set { Emp.FirstName = value; }
         }
-        public string LastName
+        public string LastName 
         {
             get { return Emp.LastName; }
             set { Emp.LastName = value; }
